9ff2cfd7aec3307eacc050b662088c3223c989f7
YUREKAN MURUGAN <144401566+YUREKAN1@users.noreply.github.com>
2026-09-12 19:09:27 +0530

    Add Easy Python Solution for 1. Two Sum from LeetCode

---
+++ LeetCode/Python/Easy/1. Two Sum/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        seen={}
+        for i , num in enumerate(nums):
+            c=target-num
+            if c in seen:
+                return [seen[c],i]
+            seen[num]=i
+        
