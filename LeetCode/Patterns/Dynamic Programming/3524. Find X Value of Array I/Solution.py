437ff5e579388bc32a3f0a862656c1b47a99f10a
YUREKAN MURUGAN <144401566+YUREKAN1@users.noreply.github.com>
2026-09-21 22:02:55 +0530

    Add Medium Python Solution for 3524. Find X Value of Array I from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/3524. Find X Value of Array I/Solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def resultArray(self, A, k):
+        res = freq = [0] * k
+
+        for n in A:
+            n %= k
+            cur = [0] * k
+            cur[n] = 1
+
+            for x, y in enumerate(freq):
+                cur[x * n % k] += y
+
+            freq = cur
+            for x, y in enumerate(freq):
+                res[x] += y
+
+        return res
+        
