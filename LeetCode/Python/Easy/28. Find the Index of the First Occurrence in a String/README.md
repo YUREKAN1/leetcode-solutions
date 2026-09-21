093f68840789904aafb4776d25c15131bb114096
YUREKAN MURUGAN <144401566+YUREKAN1@users.noreply.github.com>
2026-09-21 18:22:04 +0530

    Add Easy Python Solution for 28. Find the Index of the First Occurrence in a String from LeetCode

--- LeetCode/Python/Easy/28. Find the Index of the First Occurrence in a String/README.md
+++ LeetCode/Python/Easy/28. Find the Index of the First Occurrence in a String/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String, String Matching, Z Algorithm, Knuth–Morris–Pratt Algorithm, Boyer–Moore String-Search Algorithm
 
 ### 🚀 Performance
-- **Runtime:** 47 ms
-- **Memory:** 12.8 MB
+- **Runtime:** 0 ms
+- **Memory:** 12.3 MB
 
 ---
 
--- LeetCode/Python/Easy/28. Find the Index of the First Occurrence in a String/Solution.py
+++ LeetCode/Python/Easy/28. Find the Index of the First Occurrence in a String/Solution.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def strStr(self, haystack, needle):
-        if len(haystack)<len(needle):
-            return -1
-        for i in range(len(haystack)):
-            if haystack[i:i+len(needle)]==needle:
-                return i
-        return -1
+        return haystack.find(needle)
 
         
