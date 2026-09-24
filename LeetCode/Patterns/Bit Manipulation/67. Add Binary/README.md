38bab3538ccc6c6b521a0d3b0dd0dbadb0aff5cd
YUREKAN MURUGAN <144401566+YUREKAN1@users.noreply.github.com>
2026-09-24 10:42:28 +0530

    Add Easy Python Solution for 67. Add Binary from LeetCode

--- LeetCode/Patterns/Bit Manipulation/67. Add Binary/README.md
+++ LeetCode/Patterns/Bit Manipulation/67. Add Binary/README.md
@@ -8,8 +8,8 @@
 Math, String, Bit Manipulation, Simulation
 
 ### 🚀 Performance
-- **Runtime:** 5 ms
-- **Memory:** 12.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Python/Easy/67. Add Binary/Solution.py
+++ LeetCode/Python/Easy/67. Add Binary/Solution.py
@@ -5,18 +5,7 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        s=[]
-        carry=0
-        i=len(a)-1
-        j=len(b)-1
-
-        while i>=0 or j>=0 or carry:
-            if i>=0:
-                carry+=int(a[i])
-                i-=1
-            if j>=0:
-                carry+=int(b[j])
-                j-=1
-            s.append(str(carry%2))
-            carry//=2
-        return ''.join(reversed(s))
+        x=int(a,2)
+        y=int(b,2)
+        sum=x+y
+        return bin(sum)[2:]
